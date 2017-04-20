2c55cd2f5ffcf4f68faf8539b8adcc773c522fb3
oleg <oleg@insperedia.com>
2017-04-20 16:33:24 +0200

    Tests

--- src/com/nvlad/yii2support/validation/ValidationCompletionContributor.java
+++ src/com/nvlad/yii2support/validation/ValidationCompletionContributor.java
@@ -1,22 +1,21 @@
-package com.nvlad.yii2support.objectfactory;
+package com.nvlad.yii2support.validation;
 
 import com.intellij.codeInsight.completion.CompletionType;
 import com.intellij.patterns.ElementPattern;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.jetbrains.php.lang.PhpLanguage;
 import com.jetbrains.php.lang.psi.elements.ArrayCreationExpression;
-import com.jetbrains.php.lang.psi.elements.MethodReference;
-import com.jetbrains.php.lang.psi.elements.NewExpression;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
-import com.jetbrains.php.lang.psi.elements.impl.ArrayCreationExpressionImpl;
 import com.nvlad.yii2support.common.Patterns;
+import com.nvlad.yii2support.objectfactory.ObjectFactoryCompletionProvider;
 import org.jetbrains.annotations.NotNull;
 
-public class ObjectFactoryCompletionContributor extends com.intellij.codeInsight.completion.CompletionContributor {
-    public ObjectFactoryCompletionContributor() {
-        extend(CompletionType.BASIC, ElementPattern(), new ObjectFactoryCompletionProvider());
+/**
+ * Created by oleg on 20.04.2017.
+ */
+public class ValidationCompletionContributor extends  com.intellij.codeInsight.completion.CompletionContributor {
+    public ValidationCompletionContributor() {
+        extend(CompletionType.BASIC, ElementPattern(), new ValidationCompletionProvider());
     }
 
     @Override
---
+++ src/com/nvlad/yii2support/validation/ValidationCompletionProvider.java
@@ -0,0 +1,19 @@
+package com.nvlad.yii2support.validation;
+
+import com.intellij.codeInsight.completion.CompletionParameters;
+import com.intellij.codeInsight.completion.CompletionProvider;
+import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.ProcessingContext;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by oleg on 20.04.2017.
+ */
+public class ValidationCompletionProvider extends CompletionProvider<CompletionParameters> {
+    @Override
+    protected void addCompletions(@NotNull CompletionParameters completionParameters, ProcessingContext processingContext, @NotNull CompletionResultSet completionResultSet) {
+        PsiElement position = completionParameters.getPosition();
+        position = position;
+    }
+}
